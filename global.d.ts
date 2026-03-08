d14dec37e1d4579fb35041d449d328f40b1129ca
kaz_mighty <kaz8032@gmail.com>
2026-03-08 22:01:28 +0900

    型定義を追加し、IDEで補完が機能しやすいようにする

---
+++ global.d.ts
@@ -0,0 +1,7 @@
+/* external library */
+
+declare var Vue: typeof import("vue");
+declare var Decimal: typeof import("break_infinity.js").default;
+declare var deepmerge: typeof import("deepmerge");
+declare function isPlainObject(o: any): boolean;
+

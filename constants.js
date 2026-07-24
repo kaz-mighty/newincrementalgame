a5089d972a85d28827c88df730ae8326504a8343
kaz_mighty <kaz8032@gmail.com>
2026-07-24 16:23:51 +0900

    グローバル定数を新規ファイルに抽出

---
+++ constants.js
@@ -0,0 +1,7 @@
+const version = 2;
+const trophynum = 12;
+const setchipkind = 10;
+const setchipnum = 100;
+const ringmissionnum = 15;
+
+const worldnum = 12
--- game.js
+++ game.js
@@ -1,11 +1,3 @@
-const version = 2;
-const trophynum = 12;
-const setchipkind = 10;
-const setchipnum = 100;
-const ringmissionnum = 15;
-
-const worldnum = 12
-
 /** @type {() => PlayerSaveData} */
 const initialData = () => {
   return {
